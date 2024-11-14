3bcfecc637fed1e98d425cf3374252da2b963a5d
Thushan W A R T <rusiththarindu2@gmail.com>
2024-11-14 16:42:02 +0530

    fix: error fix

--- src/app/utils/test-analytics.ts
+++ src/app/utils/test-analytics.ts
@@ -1,9 +1,19 @@
+// export const testAnalytics = () => {
+//     if (typeof window !== 'undefined') {
+//     window.dataLayer.push({
+//     event: 'telegram_user_data',
+//     userId: 'test-user',
+//     region: 'test-region'
+//     })
+//     }
+//     }
+
 export const testAnalytics = () => {
-    if (typeof window !== 'undefined') {
-    window.dataLayer.push({
-    event: 'telegram_user_data',
-    userId: 'test-user',
-    region: 'test-region'
-    })
+    if (typeof window !== 'undefined' && Array.isArray(window.dataLayer)) {
+        window.dataLayer.push({
+            event: 'telegram_user_data',
+            userId: 'test-user',
+            region: 'test-region'
+        });
     }
-    }
+};
